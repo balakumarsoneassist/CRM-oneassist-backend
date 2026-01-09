bd68874c171fecf313e6d9ee046bab6012b34c9a
Harigaran Ramesh <harigaran.r@oneassist.net>
2026-01-09 16:13:01 +0530

    Finalize segregation route resolves

--- src/routers/segregation.router.js
+++ src/routers/segregation.router.js
@@ -8,7 +8,10 @@ router.use(verifyToken);
 router.get("/customers/all", customerController.getAllCustomers);
 router.post("/customers/reassign/:id", customerController.reassignCustomer);
 router.post("/customers/bulk-assign", customerController.bulkAssignUnassigned);
-router.post("/customers/reassign-to-employee", customerController.reassignToEmployee);
+router.post(
+  "/customers/reassign-to-employee",
+  customerController.reassignToEmployee
+);
 router.get("/customers/:id/timeline", customerController.getTimeline);
 
 module.exports = router;
